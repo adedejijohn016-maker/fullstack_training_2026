192618496e23061e8a40c062690e2a3ea979fb0a
unknown <adedejijohn016@gmail.com>
2026-06-05 01:53:02 +0100

    assignment on js

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+const score = 72;
+
+if (score >= 90) {
+  console.log("A - Excellent");
+} else if (score >= 80) {
+  console.log("B - Good");
+} else if (score >= 70) {
+  console.log("C - Satisfactory");
+} else if (score >= 60) {
+  console.log("D - Needs Improvement");
+} else {
+  console.log("F - Please see your teacher");
+}
+
